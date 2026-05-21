c040c6da9cd3fd84c44e60266a73c0ee112f204e
Islam <islam@inaya.edu.sa>
2026-05-21 15:01:02 +0300

    absolute path

--- src/index.php
+++ src/index.php
@@ -1,7 +1,10 @@
 <?php
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/mail.php';
-
+set_time_limit(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
 $message = $_SESSION['message'] ?? '';
